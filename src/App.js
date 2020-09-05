eed4008aef050a4a45b267100d85ec890e6b9570
sid2021 <wolanski.jakub@gmail.com>
2020-09-05 13:02:13 +0200

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import marked from "marked";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+marked.setOptions({
+  breaks: true,
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ markdown: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="heading">Markdown Previewer</h1>
+        <div id="wrapper">
+          <div className="editor-box">
+            <h2 className="subheading">Markdown Input</h2>
+            <textarea
+              id="editor"
+              type="text"
+              className="input"
+              value={this.state.markdown}
+              onChange={this.handleChange}
+            ></textarea>
+          </div>
+          <div className="preview-box">
+            <h2 className="subheading">Preview</h2>
+            <div
+              id="preview"
+              dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+            ></div>
+          </div>
+        </div>
+        <footer>
+          by{" "}
+          <a
+            href="https://codepen.io/sid2021"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            sid2021{" "}
+          </a>
+        </footer>
+      </div>
+    );
+  }
 }
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
 
 export default App;
