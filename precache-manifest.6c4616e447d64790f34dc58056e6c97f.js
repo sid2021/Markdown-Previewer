089e7f3d18d89ea46da2a5e75dd99e47627fbb74
sid2021 <wolanski.jakub@gmail.com>
2020-09-05 14:10:15 +0200

    Updates

---
+++ precache-manifest.6c4616e447d64790f34dc58056e6c97f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3d9729c73227ac2a5fddf12922e784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "457fc97bddd15447ceda",
+    "url": "/static/css/main.aebf772e.chunk.css"
+  },
+  {
+    "revision": "5070b17c13fd28be2fa9",
+    "url": "/static/js/2.a8db90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8db90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457fc97bddd15447ceda",
+    "url": "/static/js/main.daf1ad46.chunk.js"
+  },
+  {
+    "revision": "78c974f494e9d3578a22",
+    "url": "/static/js/runtime-main.13e5e020.js"
+  }
+]);
